e15b923f6d83cba52663e03647cd0be99193f407
Inav Zakharchenko <zakharka2000@ya.ru>
2022-12-13 10:32:35 +0300

    добавлены файлы

---
+++ controller.py
@@ -0,0 +1,8 @@
+import view
+from logger import log
+import model
+
+
+@log
+def start():
+    pass
---
+++ view.py
@@ -0,0 +1,7 @@
+from logger import log
+
+
+@log
+def greatings():
+    '''Вывод приветствия'''
+    pass
